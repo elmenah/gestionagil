b042e79c04e3d0b24d09758ae427939f237e29ce
elmenah <ni.menag@duocuc.cl>
2025-07-10 00:59:50 -0400

    xx

--- prestamed_tsx_project/vite.config.ts
+++ prestamed_tsx_project/vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3010,
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
-});
+  build: {
+    outDir: 'dist',
+  },
+  base: './' // 👈 esto es importante si usas rutas relativas
+})
